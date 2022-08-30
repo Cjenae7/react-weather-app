a20e34d31196b1f94bb503b275d5bd99a052f0ee
Cjenae7 <104477982+Cjenae7@users.noreply.github.com>
2022-08-30 12:48:23 -0500

    Added API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project created by C.Sneed and is {""}
-        <a href="https://github.com/Cjenae7/react-weather-app" target="_blank">
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <Weather defaultCity="Dallas" />
+        <footer>
+          Created by C.Sneed and is {""}
+          <a
+            href="https://github.com/Cjenae7/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
