4a630cd51a265b0d0a8e591646c0dafa8af9c421
Cjenae7 <104477982+Cjenae7@users.noreply.github.com>
2022-08-31 18:17:22 -0500

    Added date on the weather

--- src/Weather.js
+++ src/Weather.js
@@ -1,18 +1,50 @@
 import React from "react";
 import "./Weather.css";
 import axios from "axios";
-export default function Weather() {
+import FormattedDate from "./FormattedDate";
+
+export default function Weather(props) {
+  const [weatherData,setWeatherData]=useState({ready:false});
+  const[city, setCity] = usestate(props.defaultCity);
+
+
   function handleResponse(response) {
-    console.log(response.data);
+    setWeatherData({
+     ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    }); 
   }
 
-  const apiKey = "c50b5a754f93d07aef8211ca2b9025a4";
+function handleSubmit(event) {
+  event.preventDefault();
+  search();
+}
+
+function handleCityChange(event) {
+  setCity(event.target.value);
+}
+
+
+ function search() {
+   const apiKey = "c50b5a754f93d07aef8211ca2b9025a4";
   let city = "Dallas";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(handleResponse);
-  return (
+ }
+ 
+  if (weatherData.ready) {
+    return(
+
+   
     <div className="Weather">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="row">
           <div className="col-9">
             <input
@@ -20,6 +52,7 @@ export default function Weather() {
               placeholder="Enter a city.."
               className="form-control"
               autoFocus="on"
+              onChange={handleCityChange}
             />
           </div>
           <div className="col-3">
@@ -45,7 +78,7 @@ export default function Weather() {
               className="float-left"
             />
             <div className="float-left">
-              <span className="temperature">80</span>
+              <span className="temperature">{Math.round(temperature)}</span>
               <span className="unit">°C</span>
             </div>
           </div>
@@ -61,3 +94,5 @@ export default function Weather() {
     </div>
   );
 }
+ )
+  }
