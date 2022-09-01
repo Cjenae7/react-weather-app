258c8c58a797e556b7d51830a28aee2c1f5a7681
Cjenae7 <104477982+Cjenae7@users.noreply.github.com>
2022-08-31 22:33:43 -0500

    Added Celsius/Fahrenheit

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Weather.css";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
